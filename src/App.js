d298ea862e3801c35c294e58c5276ce3350b6fb2
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-10-12 18:02:16 +0300

    Add some styling changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import Joke from "./components/Joke";
 function App() {
   return (
     <div className="App">
-      <Joke setup="How did the hacker escape the police?" punchline="He just ransomware!"/>
-      <Joke setup="Why don't pirates travel on mountain roads?" punchline="Scurvy."/>
-      <Joke setup="Why do bees stay in the hive in the winter?" punchline="Swarm."/>
-      <Joke setup="What's the best thing about Switzerland?" punchline="I don't know, but the flag is a big plus!"/>
-      <Joke punchline="It’s hard to explain puns to kleptomaniacs because 
+      <Joke
+        setup="How did the hacker escape the police?"
+        punchline="He just ransomware!" />
+      <Joke
+        setup="Why don't pirates travel on mountain roads?"
+        punchline="Scurvy." />
+      <Joke
+        setup="Why do bees stay in the hive in the winter?"
+        punchline="Swarm." />
+      <Joke
+        setup="What's the best thing about Switzerland?"
+        punchline="I don't know, but the flag is a big plus!" />
+      <Joke
+        punchline="It’s hard to explain puns to kleptomaniacs because 
 they always take things literally." />
     </div>
   );
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -3,8 +3,9 @@ import React from 'react'
 const Joke = ({setup, punchline}) => {
   return (
     <div>
-        <h2>{setup}</h2>
+        {setup && <h2>{setup}</h2>}
         <h3>{punchline}</h3>
+        <hr />
     </div>
   )
 }
